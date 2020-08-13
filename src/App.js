bf28e6ccb2e969e5521f06c72a463405b604a787
d0561910 <d0561910@mail.fcu.edu.tw>
2020-08-13 14:55:17 +0800

    update version

--- src/App.js
+++ src/App.js
@@ -1,47 +1,49 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Header from './components/layout/Header';
-import Todos from './components/Todos';
-import AddTodo from './components/AddTodo';
-import About from './components/pages/About';
-import moment from 'moment';
-import Axios from 'axios'
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+import About from "./components/pages/About";
+import moment from "moment";
+import Axios from "axios";
+import "./App.css";
 
 class App extends Component {
   state = {
-    todos: []
+    todos: [],
   };
 
-  componentDidMount(){
-    Axios.get("https://jsonplaceholder.typicode.com/todos").then(response => this.setState({ todos: response.data }))
+  componentDidMount() {
+    Axios.get("https://jsonplaceholder.typicode.com/todos").then((response) =>
+      this.setState({ todos: response.data })
+    );
   }
 
   markComplete = (id) => {
     this.setState({
-      todos: this.state.todos.map(todo => {
+      todos: this.state.todos.map((todo) => {
         if (todo.id === id) {
           todo.completed = !todo.completed;
         }
         return todo;
-      })
+      }),
     });
   };
 
   delTodo = (id) => {
     this.setState({
-      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+      todos: [...this.state.todos.filter((todo) => todo.id !== id)],
     });
-  }
+  };
 
   addTodo = (title) => {
     const newTodo = {
       id: `${moment().unix()}_${Math.floor(Math.random() * 10000)}`,
       title,
-      completed: false
+      completed: false,
     };
     this.setState({ todos: [...this.state.todos, newTodo] });
-  }
+  };
 
   render() {
     return (
@@ -49,17 +51,21 @@ class App extends Component {
         <div className="App">
           <div className="container">
             <Header />
-            <Route exact path='/' render={props => (
-              <React.Fragment>
-                <AddTodo addTodo={this.addTodo} />
-                <Todos
-                  todos={this.state.todos}
-                  markComplete={this.markComplete}
-                  delTodo={this.delTodo}
-                />
-              </React.Fragment>
-            )} />
-            <Route path='/about' component={About} />
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    delTodo={this.delTodo}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route path="/about" component={About} />
           </div>
         </div>
       </Router>
